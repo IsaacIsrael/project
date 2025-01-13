875f305ad37be4ada2082586f82a2b3235a5d633
Isaac Israel <email.iisrael@gmail.com>
2025-01-13 09:37:00 +0200

    config `eslint-config-prettier` in `eslint`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -26,4 +27,10 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {},
+  },
+  // * This configuration should be in the end as mention here -
+  // * https://github.com/prettier/eslint-config-prettier?tab=readme-ov-file#what-and-why
+  eslintConfigPrettier,
 ];
