f3fb1b60979502b89627eb3c9810ec8061f160bd
Isaac Israel <email.iisrael@gmail.com>
2025-02-16 08:20:50 +0200

    docs: create `Storybook` docs page

---
+++ docs/src/pages/Storybook/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+const Storybook: React.FC = () => {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      description={'Description will go into a meta tag in <head />'}
+      title={`Storybook from ${siteConfig.title}`}
+      noFooter>
+      <main>
+        <iframe className={styles.storybook} src={'../storybook/'} />
+      </main>
+    </Layout>
+  );
+};
+
+export default Storybook;
