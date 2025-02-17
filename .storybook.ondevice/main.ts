6a0fee908653381a74e5816646acc24220d34773
Isaac Israel <email.iisrael@gmail.com>
2025-02-17 09:27:59 +0200

    Deploy website - based on 3b0c1df89f91d50377ce0954d27055c43526c36d

--- .storybook.ondevice/main.ts
+++
@@ -1,8 +0,0 @@
-import type { StorybookConfig } from '@storybook/react-native';
-
-const main: StorybookConfig = {
-  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
-  stories: ['../src/components/**/*.stories.?(ts|tsx|js|jsx)'],
-};
-
-export default main;
