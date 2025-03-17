0c14684fbae1a1b85eb228161220bcbe2a7ae268
Isaac Israel <email.iisrael@gmail.com>
2025-03-17 09:21:10 +0200

    feat: create `useAppSelector` and `useAppDispatch`

--- src/types/Infrastructure/Store.d.ts
+++ src/types/Infrastructure/Store.d.ts
@@ -1,6 +1,12 @@
 import type store from '@store';
 import type rootReducer from '@store/rootReducer';
 
+/**
+ * The type of the store's dispatch.
+ */
 export type AppDispatch = typeof store.dispatch;
 
+/**
+ * The type of the store's root state.
+ */
 export type RootState = ReturnType<typeof rootReducer>;
