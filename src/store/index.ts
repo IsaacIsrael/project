3e819cc420d185aca155ce7990f92adef16ff2c6
Isaac Israel <email.iisrael@gmail.com>
2025-03-21 10:50:10 +0200

    feat: add `reactotron` to `store`

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,18 @@
+import EnvironmentConstant from 'constants/EnvironmentConstant';
+import ReactotronManager from 'helpers/managers/ReactotronManager';
+
 import rootReducer from './rootReducer';
 
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
+  enhancers: (getDefaultEnhancers) => {
+    const enhancers = getDefaultEnhancers();
+    if (EnvironmentConstant.isLocal) {
+      enhancers.concat(ReactotronManager.createEnhancer());
+    }
+    return enhancers;
+  },
   reducer: rootReducer,
 });
 
