107884dc994eb09ce87b8f1d3da4d1292b37c214
Isaac Israel <email.iisrael@gmail.com>
2025-04-20 09:39:55 +0300

    test: add `Button` component test

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, View } from 'react-native';
 
 import MyButton from './Button';
 
+import { expect, fn, userEvent, waitFor, within } from '@storybook/test';
+
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
@@ -27,11 +29,47 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {};
+export const Basic: Story = {
+  args: {
+    onPress: fn(),
+    testID: 'my-button',
+  },
+  play: async ({ args, canvasElement, step }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.findByTestId(args.testID ?? '');
+
+    expect(button).toBeTruthy();
+    await step('button arguments', async () => {
+      expect(button).toHaveTextContent(args.text);
+    });
+
+    await step('click on button', async () => {
+      await userEvent.click(button);
+    });
+    await waitFor(() => expect(args.onPress).toHaveBeenCalled());
+  },
+};
 
 export const AnotherExample: Story = {
   args: {
+    onPress: fn(),
+    testID: 'another-button',
     text: 'Another example',
+  },
+  play: async ({ args, canvasElement, step }) => {
+    const canvas = within(canvasElement);
+    const button = await canvas.findByTestId(args.testID ?? '');
+
+    expect(button).toBeTruthy();
+
+    await step('button arguments', async () => {
+      expect(button).toHaveTextContent(args.text);
+    });
+
+    await step('click on button', async () => {
+      await userEvent.click(button);
+    });
+    await waitFor(() => expect(args.onPress).toHaveBeenCalled());
   },
 };
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,11 +4,12 @@ import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 export type MyButtonProps = {
   readonly onPress?: () => void;
+  readonly testID?: string;
   readonly text: string;
 };
 
-const MyButton: React.FC<MyButtonProps> = ({ onPress, text }: MyButtonProps) => (
-  <TouchableOpacity activeOpacity={0.8} style={styles.container} onPress={onPress}>
+const MyButton: React.FC<MyButtonProps> = ({ onPress, testID, text }: MyButtonProps) => (
+  <TouchableOpacity activeOpacity={0.8} style={styles.container} testID={`${testID}`} onPress={onPress}>
     <Text style={styles.text}>{text}</Text>
   </TouchableOpacity>
 );
