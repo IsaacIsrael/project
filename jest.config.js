e1c0fbbbe9aada5a78c60ffbf18135e851c13932
Isaac Israel <email.iisrael@gmail.com>
2025-05-23 16:13:15 +0300

    test: config `jest-watch-select-projects`

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname', 'jest-watch-select-projects'],
   projects: ['./test/jest.integration.config.js', './test/jest.logic.config.js'],
 };
