034ee93a3d8942a0786b05b1ff57aefa45b20073
Isaac Israel <email.iisrael@gmail.com>
2025-05-26 18:58:25 +0300

    test: update jest configuration for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  ...require('./test/jest.common.config'),
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname', 'jest-watch-select-projects'],
   projects: ['./test/jest.integration.config.js', './test/jest.logic.config.js'],
 };
