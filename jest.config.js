a4588e358a4f9066f0c9fe6ce74b19f68974cbab
Isaac Israel <email.iisrael@gmail.com>
2025-06-27 10:40:59 +0300

    chore: undu

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@ module.exports = {
   ...require('./test/jest.common.config'),
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname', 'jest-watch-select-projects'],
   projects: ['./test/jest.integration.config.js', './test/jest.logic.config.js'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        classNameTemplate: 'DEBUG: {filename}-{classname}-{filepath}',
+      },
+    ],
+  ],
 };
